cc3c31b5d041f7755016efaa9c5c396e8596c477
Klim Naydenov <knaydenov@gmail.com>
2019-05-29 23:27:43 +0300

    Fix issues

--- projects/hal-tree/src/public_api.ts
+++ projects/hal-tree/src/public_api.ts
@@ -5,5 +5,4 @@ export * from './lib/hal-tree/hal-tree.component';
 export * from './lib/hal-tree-action-def.directive';
 export * from './lib/hal-tree-action.directive';
 export * from './lib/hal-tree-data-source';
-export * from './lib/hal-tree-item';
 export * from './lib/hal-tree.module';
